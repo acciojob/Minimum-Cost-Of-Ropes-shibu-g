51d31a7f67748516558c011cf6d73b64a6227f29
shibu-g <109884948+shibu-g@users.noreply.github.com>
2023-02-14 20:47:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your code here
+function fun(even) {
+	even.preventDefault();
+var inp=document.querySelector('input').value;
+var arr=inp.split(",");
+	arr.sort(function(a,b){return a-b});
+	let cost=0;
+	while (arr.length>1) {
+	let curr=Number(arr[0])+Number(arr[1]);
+	arr.splice(0,2);
+	arr.push(curr);
+		cost+=curr;
+		arr.sort(function(a,b){return a-b});
+	}
+	var ans=document.getElementById('result').textContent=cost;
+}
